921b9a0e2a14ca6d6ebc9a9cf797f1e9ec6478d0
Malini Krishnamurthy <72509011+malinikrishnamurthy-14@users.noreply.github.com>
2020-11-24 12:00:38 +0530

    react dev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import DFirm from './pages/DFirm'; //home page
+import AboutUs from './pages/AboutUs';
+import ServicesList from './pages/ServicesList';
+import Service from './pages/Service';
+import ContactUs from './pages/ContactUs';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavigationBar from './NavigationBar.js';
+import ContentArea from './ContentArea.js';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <NavigationBar />
+          <ContentArea />
+
+          <Switch>
+            <Route path="/" component={DFirm} exact/>
+            <Route path="/about-us" component={AboutUs} />
+            <Route path="/services-list" component={ServicesList} />
+            <Route path="/service/:name" component={Service} />
+            <Route path="/contact-us" component={ContactUs} />
+            <Route component={NotFound} />
+          </Switch>
+
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
